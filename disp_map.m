645e478601cbdf9561c2c3aec54871ef8de488e7
LiamRattray <ltrattray@dundee.ac.uk>
2019-03-19 18:37:34 +0000

    fixed the thing

--- disp_map.m
+++ disp_map.m
@@ -1,10 +1,12 @@
-function [disparity_mapX, disparity_mapY] = disp_map(img1, img2, support_window_size, search_area_size)
+function [disparity_mapX, disparity_mapY, disparity_map] = disp_map(img1, img2, support_window_size, search_area_size)
     %image size
+    img1 = rgb2gray(img1);
+    img2 = rgb2gray(img2);
     [imgsizeX, imgsizeY, imgsizeZ] = size(img1);
     % create empty disparity map
     disparity_mapX = zeros(imgsizeX, imgsizeY);
     disparity_mapY = zeros(imgsizeX, imgsizeY);
-    %disparity_map = cell(imgsizeX, imgsizeY);
+    disparity_map = cell(imgsizeX, imgsizeY);
 
     % check that search area size is smaller than image
     
@@ -17,7 +19,11 @@ function [disparity_mapX, disparity_mapY] = disp_map(img1, img2, support_window_
     for i = 1:imgsizeX
         for j = 1:imgsizeY
             [ssd, xa, ya, d] = pixel_disp(img1, img2, i, j, support_window_size, search_area_size);
+            %movement along up - down
             disparity_mapX(i, j) = i-xa;
+            %movement along left - right
             disparity_mapY(i, j) = j-ya;
+            %stores vectors and values
+            disparity_map{i, j} = [d, ssd];
         end
     end
--- top_left.m
+++ top_left.m
@@ -1,5 +1,5 @@
 function [corx, cory] = top_left(x, y, winsize)
-    top = y - winsize/2;
-    left = x - winsize/2;
+    top = floor(y - winsize/2);
+    left = floor(x - winsize/2);
     corx=left;
     cory=top;
