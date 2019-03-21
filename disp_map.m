6e752a1793ba89d38fc4c3ed94ae8112164fb9c3
Liu-ko <33573204+Liu-ko@users.noreply.github.com>
2019-03-21 20:55:45 +0000

    searching window is made flexible

--- disp_map.m
+++ disp_map.m
@@ -1,4 +1,4 @@
-function [disparity_mapX, disparity_mapY, disparity_map] = disp_map(img1, img2, support_window_size, search_area_h_size, search_area_v_size)
+function [disparity_map] = disp_map(img1, img2, support_window_size, search_area_h_size, search_area_v_size)
     %try to convert RGB images into grey one 
     try
         img1 = rgb2gray(img1);
@@ -8,23 +8,18 @@ function [disparity_mapX, disparity_mapY, disparity_map] = disp_map(img1, img2, 
     %get image size
     [imgsizeY, imgsizeX] = size(img1);
     % create empty disparity map
-    disparity_mapY = zeros(imgsizeY, imgsizeX);
-    disparity_mapX = zeros(imgsizeY, imgsizeX);
-    disparity_map = cell(imgsizeY, imgsizeX);
+    disparity_map = zeros(imgsizeY, imgsizeX);
     
     f = waitbar(0, "Generating disparity map");
 
     %loot through the image calculating the disparity map
-    for i = 30:imgsizeX-30
+    for i = 1:imgsizeX
         waitbar(i/imgsizeX, f, "Generating disparity map");
-        for j = 30:imgsizeY-30
+        for j = 1:imgsizeY
             [ssd, xa, ya, d] = pixel_disp(img1, img2, i, j, support_window_size, search_area_h_size, search_area_v_size);
             %movement along left/right
-            disparity_mapX(j, i) = i-xa;
-            %movement along up/down
-            disparity_mapY(j, i) = j-ya;
-            %stores vectors and values
-            disparity_map{j, i} = [d, ssd];
+            disparity_map(j, i) = i-xa;
+            search_area_h_size = abs(i-xa)*2 + 2;
         end
     end
     
